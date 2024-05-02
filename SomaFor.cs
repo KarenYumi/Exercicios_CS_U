d60cbc75fdd3208943561e3b983be60a8f9cc076
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-05-02 19:39:51 -0300

    Add files via upload

---
+++ SomaFor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto2
+{
+     class SomaFor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números inteiros será somado? ");
+            int n = int.Parse(Console.ReadLine());
+            int soma = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Numero" + i + ":");
+                int num = int.Parse(Console.ReadLine());
+                soma += num;
+            }
+            
+            Console.WriteLine("A soma é de" + soma);
+        }
+    }
+}
