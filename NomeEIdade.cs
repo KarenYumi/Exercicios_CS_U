36f173bbfa94d0e07b11aacabe1eb8bd0a3b2609
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-05-21 21:26:00 -0300

    Update NomeEIdade.cs

--- NomeEIdade.cs
+++ NomeEIdade.cs
@@ -1,41 +1,41 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Courser
-//{
-//    internal class NomeEIdade
-//    {
-//        //static void Main(string[] args)
-//        //{
-//        //    ClasseNomeIDade x;
-//        //    x = new ClasseNomeIDade();
-//        //    double maior = 0;
-//        //    string NomeMaior="";
+namespace Courser
+{
+    internal class NomeEIdade
+    {
+        static void Main(string[] args)
+        {
+            ClasseNomeIDade x;
+            x = new ClasseNomeIDade();
+            double maior = 0;
+            string NomeMaior="";
 
-//        //    Console.WriteLine("Informe o número de pessoas intrevistadas:");
-//        //    int numIntrevistadas = int.Parse(Console.ReadLine());
-//        //    for (int i = 1; i <= numIntrevistadas; i++)
-//        //    {
-//        //        Console.WriteLine("O nome "+ i + " pessoa:");
-//        //        x.nome = Console.ReadLine();
+            Console.WriteLine("Informe o número de pessoas intrevistadas:");
+            int numIntrevistadas = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numIntrevistadas; i++)
+            {
+                Console.WriteLine("O nome "+ i + " pessoa:");
+                x.nome = Console.ReadLine();
 
-//        //        Console.WriteLine("Idade: ");
-//        //        x.idade = double.Parse(Console.ReadLine());
+                Console.WriteLine("Idade: ");
+                x.idade = double.Parse(Console.ReadLine());
                 
                 
-//        //        if (x.idade > maior)
-//        //        {
-//        //            maior = x.idade;
-//        //            NomeMaior = x.nome;
+                if (x.idade > maior)
+                {
+                    maior = x.idade;
+                    NomeMaior = x.nome;
                     
-//        //        }
+                }
                 
-//        //    }
-//        //    Console.WriteLine("A pessoa mais velha: " + NomeMaior);
+            }
+            Console.WriteLine("A pessoa mais velha: " + NomeMaior);
 
-//        }
-//    }
-//}
+        }
+    }
+}
