1abb9c3a80d985a8eb143829f49d20a0691b3999
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-06-12 18:40:08 -0300

    Update AreaMaiorTriangulo.cs
    Atualização usando o método dentro da classe

--- AreaMaiorTriangulo.cs
+++ AreaMaiorTriangulo.cs
@@ -15,16 +15,14 @@ namespace Courser
              x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
              x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
              x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p-x.A)*(p-x.B)*(p-x.C));
-            
-
+        
             Console.WriteLine("Insira as 3 medidas do triângulo Y: ");
             y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            p = (y.A +y.B +y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            
+            double areaX = x.Area();
+            double areaY = y.Area();
 
             Console.WriteLine("Área do X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área do Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
