04010f93c79bf57d593721c3a45678fa9fac17e0
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-06-12 18:35:33 -0300

    Update Triangulo.cs

--- Triangulo.cs
+++ Triangulo.cs
@@ -1,9 +1,15 @@
-﻿namespace Courser
+using System;
+namespace Courser
 {
     class Triangulo
     {
         public double A;
         public double B;    
         public double C;
+
+        public double Area(){
+            double p = ( A + B + C)/2.0;
+            return Math.Sqrt(p*(p - A) * (p - B) * (p - C));
+        }    
     }
 }
