5f42a50aa2ab77f336eb5dac4d0fb12f296b6d26
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-06-13 17:24:27 -0300

    Add files via upload
    Exercício de ToString e uso de método.
    Ele recebe um porduto, seu valor e a quantidade, em seguida pode adicionar e remover a quantidade dos produtos.

---
+++ ClasseEstoque.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Courser
+{
+    class ClasseEstoque
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //dado de entrada a ser add (quantidade) como parâmetro de entrada
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$" + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Estoque.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courser
+{
+     class Estoque {
+        static void Main(string[] args) {
+
+            ClasseEstoque p = new ClasseEstoque();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDados do produto: " + p);
+
+            Console.Write("\n\nDigite o número de produtos a ser adicionado ao estoque: " );
+            int quant = int.Parse(Console.ReadLine());
+            // chamamos a operação AddProduto que é capaz de receber o valor como entrada (quant) e armazenar na quantidade dentro da Classe
+            p.AdicionarProdutos(quant);
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.Write("\nDigite o número de produtos a ser removido do estoque: ");
+            quant = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(quant);
+            Console.WriteLine("Dado atualizados: " + p);
+            Console.WriteLine();
+
+
+        }
+    }
+}
