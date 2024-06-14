a5900861ff1c7d139f846ffb9efb4c22b09785aa
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-06-14 16:38:59 -0300

    Add files via upload
    Pega  notas de um aluno e motra se o aluno está aprovado ou reprovado. Estudo prático de classe.

---
+++ ClasseNotaAluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courser
+{
+    internal class ClasseNotaAluno
+    {
+        public string Nome;
+        public double NotaA;
+        public double NotaB;
+        public double NotaC;
+        
+
+        public double Media()
+        {
+            return  NotaA + NotaB  + NotaC;
+        }
+        public double NotaFalta()
+        {
+            return  60.00 - Media();
+        }
+    }
+}
---
+++ NotaAluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Courser
+{
+    internal class NotaAluno
+    {
+        static void Main(string[] args)
+        {
+            ClasseNotaAluno p = new ClasseNotaAluno();
+
+            Console.Write("Nome do Aluno: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            p.NotaA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.NotaB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.NotaC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("\nNota Final = " + p.Media().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (p.Media() > 60.00)
+            {
+                Console.WriteLine("\nAPROVADO");
+            }
+            else
+            {
+                Console.WriteLine("\nREPROVADO");
+                Console.WriteLine("Faltaram " + p.NotaFalta().ToString("F2", CultureInfo.InvariantCulture)+ " pontos");
+            }
+
+        }
+        
+    }
+}
