2d9893a34114175fb73873575ab2216cd8179573
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-06-14 16:03:43 -0300

    Add files via upload
    Recebe nome, sal bruto e imposto e depois pede uma porcentagem para aumentar o salário. Estudo de fixação de classe.

---
+++ ClasseDadosFuncionarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courser
+{
+    class ClasseDadosFuncionarios
+    {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return Salario - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario = Salario + (Salario * porcentagem / 100.00);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$" + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ DadosFuncionarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Courser
+{
+    internal class DadosFuncionarios
+    {
+        static void Main(string[] args)
+        {
+            ClasseDadosFuncionarios p = new ClasseDadosFuncionarios();
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            p.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            p.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + p);
+
+            Console.Write("\nDigite a porcentagem para aumentar o salário: ");
+            double porc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Lembrar: puxar a classe e colocar o readline dentro do parêntese;
+            p.AumentarSalario(porc);
+
+            Console.WriteLine("\nDados Atualizados: " + p);
+
+
+
+        }
+    }
+}
