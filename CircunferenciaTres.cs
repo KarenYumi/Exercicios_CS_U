552b6f7a71683494ed0827bfb2e8b11f73428c4a
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-06-20 18:09:57 -0300

    Explicação
    usando membros estático, para calcular as medidas de um circulo.

---
+++ CircunferenciaTres.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Courser
+{
+    internal class CircunferenciaTres
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = ClasseCircunferenciaTres.Circunfe(raio);
+            double volume = ClasseCircunferenciaTres.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + ClasseCircunferenciaTres.pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        
+    }
+}
---
+++ ClasseCircunferenciaTres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courser
+{
+    internal class ClasseCircunferenciaTres
+    {
+        
+        
+        public static double pi = 3.14;
+
+        public static double Circunfe(double r)
+        {
+            return 2.0 * pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * pi * Math.Pow(r, 3.0);
+        }
+        
+    }
+}
