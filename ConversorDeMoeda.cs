065c8f0dac108ad02ce0cc03e6b3bc9c0e92f251
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-06-21 15:38:45 -0300

    Explicação 3
    usando Método Estático, calculei o valor de quantos deve pagar a comprar uma qunatidade de dolar,

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courser
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 0.06;
+        public static double Conversor(double dolar, double reais)
+        {
+            double Total = reais * dolar;
+            return Total + (IOF * Total);
+        }
+        
+    }
+}
---
+++ CotacaoDeDolar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Courser
+{
+    internal class CotacaoDeDolar
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual é a cotação do dolar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você cai comprar? ");
+            double reais = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.Conversor(dolar, reais);
+            Console.WriteLine("Valor a ser pago em reais= " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
